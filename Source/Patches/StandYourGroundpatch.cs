719b01661037dd24f27d3a7fc260f8f5d023841e
Michal Fouquette <michal.fouquette@gmail.com>
2025-10-03 19:34:04 -0700

    Add settings menu via XMLExtensions

--- Source/Patches/StandYourGroundpatch.cs
+++ Source/Patches/StandYourGroundpatch.cs
@@ -1,6 +1,8 @@
+using System;
 using RimWorld;
 using Verse;
 using HarmonyLib;
+using XmlExtensions;
 
 namespace MadagascarVanilla.Patches
 {
@@ -10,38 +12,29 @@ namespace MadagascarVanilla.Patches
     public static class StandYourGroundPatch
     {
         // by performing the same checks as AddAndRemoveDynamicComponents does, instead of it
-        // creating the playersettings when they are null, we should be doing so (and thus preventing it from ever doing so)
+        // creating the PlayerSettings when they are null, we should be doing so (and thus preventing it from ever doing so)
         // we can then change the hostilityResponse (or other values "set" in the constructor) without having to actually
         // patch the constructor.
+        //
+        // flags on original method
+        //bool flag1 = pawn.Faction != null && pawn.Faction.IsPlayer;
+        //bool flag2 = pawn.HostFaction != null && pawn.HostFaction.IsPlayer;
+        // ...
+        // later
+        // ...
+        // if ((flag1 | flag2 || pawn.IsOnHoldingPlatform) && pawn.playerSettings == null)
+        //    pawn.playerSettings = new Pawn_PlayerSettings(pawn);
         public static void Prefix(ref Pawn pawn)
         {
-            // flags on original method
-            //bool flag1 = pawn.Faction != null && pawn.Faction.IsPlayer;
-            //bool flag2 = pawn.HostFaction != null && pawn.HostFaction.IsPlayer;
-            
-            // later
-            
-            // if ((flag1 | flag2 || pawn.IsOnHoldingPlatform) && pawn.playerSettings == null)
-            //    pawn.playerSettings = new Pawn_PlayerSettings(pawn);
-            
-            
-            //FileLog.Log("syg: prefix response enter");
-    
-             var original = typeof(PawnComponentsUtility).GetMethod("AddAndRemoveDynamicComponents");
-    
              bool pawn_faction_is_player = pawn.Faction != null && pawn.Faction.IsPlayer;
              bool pawn_host_faction_is_player = pawn.HostFaction != null && pawn.HostFaction.IsPlayer;
     
              if ((pawn_faction_is_player || pawn_host_faction_is_player || pawn.IsOnHoldingPlatform) && pawn.playerSettings == null)
              {
-                 //FileLog.Log("syg: creating playersettings");
                  pawn.playerSettings = new Pawn_PlayerSettings(pawn);
-    
-                 //FileLog.Log("syg: changing hostilityResponse to attack");
-                 // FIXME: use the setting instead of changing it to Attack. 
-                 pawn.playerSettings.hostilityResponse = HostilityResponseMode.Attack;
                  
-                 //pawn.playerSettings.hostilityResponse = MadagascarVanillaSettings.default_hostility_response;
+                 string hostilityResponse = (SettingsManager.GetSetting("protobeard.madagascarvanilla", "hostilityResponse"));
+                 pawn.playerSettings.hostilityResponse = (HostilityResponseMode) Enum.Parse(typeof(HostilityResponseMode), hostilityResponse);
              }
         }
     }
