cc4e1bfe51f3f08dc88cbf678a64d920ff039075
Michal Fouquette <michal.fouquette@gmail.com>
2025-10-07 14:52:10 -0700

    Handle incapable of violence pawns in StandYourGround patch

--- Source/Patches/StandYourGroundPatch.cs
+++ Source/Patches/StandYourGroundPatch.cs
@@ -12,6 +12,7 @@ namespace MadagascarVanilla.Patches
     public static class StandYourGroundPatch
     {
         private const string HostilityRewardsPreference = "hostilityResponse";
+        
         // by performing the same checks as AddAndRemoveDynamicComponents does, instead of it
         // creating the PlayerSettings when they are null, we should be doing so (and thus preventing it from ever doing so)
         // we can then change the hostilityResponse (or other values "set" in the constructor) without having to actually
@@ -32,10 +33,14 @@ namespace MadagascarVanilla.Patches
     
              if ((pawnFactionIsPlayer || pawnHostFactionIsPlayer || pawn.IsOnHoldingPlatform) && pawn.playerSettings == null)
              {
-                 pawn.playerSettings = new Pawn_PlayerSettings(pawn);
+                 HostilityResponseMode hostilityResponse = (HostilityResponseMode) Enum.Parse(typeof(HostilityResponseMode), (SettingsManager.GetSetting(MadagascarVanillaMod.ModId, HostilityRewardsPreference)));
                  
-                 string hostilityResponse = (SettingsManager.GetSetting(MadagascarVanillaMod.ModId, HostilityRewardsPreference));
-                 pawn.playerSettings.hostilityResponse = (HostilityResponseMode) Enum.Parse(typeof(HostilityResponseMode), hostilityResponse);
+                 pawn.playerSettings = new Pawn_PlayerSettings(pawn);
+                 pawn.playerSettings.hostilityResponse = hostilityResponse;
+                 
+                 // If the pawn is incapable of violence and we just set them to Attack, grab the next response mode in the list and set them to that instead.
+                 if (pawn.WorkTagIsDisabled(WorkTags.Violent) && hostilityResponse == HostilityResponseMode.Attack)
+                     pawn.playerSettings.hostilityResponse = HostilityResponseModeUtility.GetNextResponse(pawn);
              }
         }
     }
