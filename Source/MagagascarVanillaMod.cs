3df4ed6b73ac67f3cb9d23b3e6b241049259b251
Michal Fouquette <michal.fouquette@gmail.com>
2025-10-07 14:31:13 -0700

    Remove redundant settings code
    Since we're using XML Extensions for settings, no need to have the vestigial stuff. Can always add it back later if we need some type of setting that XML Extensions can't handle.

--- Source/MagagascarVanillaMod.cs
+++ Source/MagagascarVanillaMod.cs
@@ -6,21 +6,8 @@ namespace MadagascarVanilla
     public class MadagascarVanillaMod : Mod
     {
         public const string ModId = "com.protobeard.madagascarvanilla";
-        public static MadagascarVanillaSettings settings;
         
         public MadagascarVanillaMod(ModContentPack content) : base(content) {
-            settings = GetSettings<MadagascarVanillaSettings>();
-        }
-
-        public override string SettingsCategory()
-        {
-            return "MadagascarVanilla.Settings".Translate();
-        }
-        
-        public override void DoSettingsWindowContents(Rect inRect)
-        {
-            // FIXME: Just get rid of this in favor of XML Extensions settings?
-            // Doesn't really seem like two types of settings make much sense, esp. since I don't have any yet.
         }
     }
 }
