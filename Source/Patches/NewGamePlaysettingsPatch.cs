aa76fad08bb4cf580f1999084295b4bca57bff56
Michal Fouquette <michal.fouquette@gmail.com>
2025-10-10 20:43:21 -0700

    Add setting for defaulting to manual work priorities

--- Source/Patches/NewGamePlaysettingsPatch.cs
+++ Source/Patches/NewGamePlaysettingsPatch.cs
@@ -15,6 +15,7 @@ namespace MadagascarVanilla.Patches
         public const string LandmarkVisibilitySetting = "showExpandingLandmarks";
         public const string AutoHomeAreaSetting = "autoHomeArea";
         public const string AutoRebuildSetting = "autoRebuild";
+        public const string UseWorkPrioritiesSetting = "useWorkPriorities";
         
         [HarmonyPostfix]
         [HarmonyPatch(typeof(PlaySettings))]
@@ -25,9 +26,13 @@ namespace MadagascarVanilla.Patches
             bool autoHomeAreaSetting = bool.Parse(SettingsManager.GetSetting(MadagascarVanillaMod.ModId, AutoHomeAreaSetting));
             bool autoRebuildSetting = bool.Parse(SettingsManager.GetSetting(MadagascarVanillaMod.ModId, AutoRebuildSetting));
             
+            bool useWorkPrioritiesSetting = bool.Parse(SettingsManager.GetSetting(MadagascarVanillaMod.ModId, UseWorkPrioritiesSetting));
+            
             __instance.showExpandingLandmarks = landmarkVisibilitySetting;
             __instance.autoHomeArea = autoHomeAreaSetting;
             __instance.autoRebuild = autoRebuildSetting;
+            
+            __instance.useWorkPriorities = useWorkPrioritiesSetting;
         }
     }
 }
