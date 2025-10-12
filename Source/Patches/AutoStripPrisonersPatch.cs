ad50f1311a6d4c0d2939f6b0778a471e3174c855
protobeard <michal.fouquette@gmail.com>
2025-10-12 11:29:16 -0700

    Add auto strip prisoner settings
    Automatically strip prisoners when captured. Also optionally auto strip colonists when arrested.

---
+++ Source/Patches/AutoStripPrisonersPatch.cs
@@ -0,0 +1,43 @@
+using RimWorld;
+using HarmonyLib;
+using Verse;
+using XmlExtensions;
+
+namespace MadagascarVanilla.Patches
+{
+    
+    [HarmonyPatch(typeof(Pawn_GuestTracker))]
+    [HarmonyPatch(nameof(Pawn_GuestTracker.SetGuestStatus))]
+    public static class AutoStripPatch
+    {
+        private const string EnableAutoStrip = "enableAutoStrip";
+        private const string EnableAutoStripArrestedColonist = "enableAutoStripArrestedColonist";
+        
+        // Automatically strip prisoners when captured. Also optionally auto strip colonists when
+        // arrested.
+        public static void Postfix(Pawn_GuestTracker __instance, Pawn ___pawn)
+        {
+            bool enableAutoStrip = bool.Parse(SettingsManager.GetSetting(MadagascarVanillaMod.ModId, EnableAutoStrip));
+            bool enableAutoStripArrestedColonist = bool.Parse(SettingsManager.GetSetting(MadagascarVanillaMod.ModId, EnableAutoStripArrestedColonist));
+            
+            // Bail if we're not dealing with a prisoner
+            if (!enableAutoStrip || !___pawn.IsPrisonerOfColony)
+                return;
+
+            // Bail if the prisoner is a colonist unless the setting to strip them too is enabled
+            if (!enableAutoStripArrestedColonist && ___pawn.IsColonist)
+                return;
+            
+            Designation stripPawnDesignation = new Designation(___pawn, DesignationDefOf.Strip);
+
+            if (___pawn.Map != null)
+            {
+                ___pawn.Map.designationManager.AddDesignation(stripPawnDesignation);
+                return;
+            }
+            
+            // Get the map from the Pawn's parent object and then add the strip designation
+            ___pawn.MapHeld?.designationManager.AddDesignation(stripPawnDesignation);
+        }
+    }
+}
