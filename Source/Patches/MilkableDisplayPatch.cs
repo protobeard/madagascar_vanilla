a5adc081cb313cd561cc73b9412d53dcfdbd303d
protobeard <michal.fouquette@gmail.com>
2025-10-12 15:21:54 -0700

    Add setting to fix incorrect Milk fullness label
    So that it says "Chemfuel" for boomalopes, etc.

---
+++ Source/Patches/MilkableDisplayPatch.cs
@@ -0,0 +1,38 @@
+using RimWorld;
+using HarmonyLib;
+using Verse;
+using XmlExtensions;
+
+namespace MadagascarVanilla.Patches
+{
+    
+    [HarmonyPatch(typeof(CompMilkable))]
+    [HarmonyPatch(nameof(CompMilkable.CompInspectStringExtra))]
+    public static class MilkableDisplayPatch
+    {
+        private const string EnableCompMilkableDisplayProperItem = "enableCompMilkableDisplayProperItem";
+        
+        public static void Postfix(CompMilkable __instance, ref string __result)
+        {
+            bool enableCompMilkableDisplayProperItem = bool.Parse(SettingsManager.GetSetting(MadagascarVanillaMod.ModId, EnableCompMilkableDisplayProperItem));
+
+            // Bail if there is no inspect string or the type of resource being produced is milk.
+            if (__result == null || __instance.Props.milkDef.IsMilk())
+                return;
+            
+            if (enableCompMilkableDisplayProperItem)
+            {
+                __result = __instance.Props.milkDef.LabelCap;
+                __result += " " + "MilkableResourceFullness".Translate() + ": ";
+                __result += __instance.Fullness.ToStringPercent();
+            }
+        }
+        
+        public static bool IsMilk(this ThingDef def)
+        {
+            if (def.label.Contains("Milk"))
+                return true;
+            return false;
+        }
+    }
+}
