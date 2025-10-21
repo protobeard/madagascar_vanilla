3a2420cf8c76b32c8c95362609c6652e21255f00
protobeard <michal.fouquette@gmail.com>
2025-10-21 15:27:07 -0700

    Make resetting settings clear default areas
    No-op out of the patch if there aren't any area names in the list (default).

--- Source/Patches/AreasPatch.cs
+++ Source/Patches/AreasPatch.cs
@@ -12,34 +12,29 @@ namespace MadagascarVanilla.Patches
     [HarmonyPatch(nameof(AreaManager.AddStartingAreas))]
     public static class AreasPatch
     {
-        private const string StartingAreasList = "startingAreasList";
+        private const string StartingAreasListKey = "startingAreasList";
         private const char StartingAreaDelimiter = ',';
         
         public static void Postfix(AreaManager __instance)
         {
-            string startingAreasString = (SettingsManager.GetSetting(MadagascarVanillaMod.ModId, StartingAreasList));
+            string startingAreasString = (SettingsManager.GetSetting(MadagascarVanillaMod.ModId, StartingAreasListKey));
             List<string> startingAreasList = startingAreasString.Split(StartingAreaDelimiter).Select(s => s.Trim()).ToList();
+
+            if (!startingAreasList.Any() || (startingAreasList.Count == 1 && startingAreasList.First().NullOrEmpty())) 
+                return;
             
-            if (startingAreasList.Any())
+            // Remove the default Area RimWorld creates ("Area 1")
+            Area defaultArea = __instance.AllAreas.Where((area => area is Area_Allowed)).FirstOrDefault();
+            defaultArea?.Delete();
+
+            // Create our desired default Areas
+            foreach (string startingAreaName in startingAreasList)
             {
-                // Remove the default Area RimWorld creates ("Area 1")
-                Area defaultArea = __instance.AllAreas.Where((area => area is Area_Allowed)).FirstOrDefault();
-                defaultArea?.Delete();
-
-                // Create our desired default Areas
-                foreach (string startingAreaName in startingAreasList)
-                {
-                    if (__instance.TryMakeNewAllowed(out Area_Allowed areaAllowed))
-                    {
-                        areaAllowed.RenamableLabel = startingAreaName;
-                    }
-                    else
-                    {
-                        Log.Error("Attempted to create more default areas than what is allowed.");
-                    }
-                }
+                if (__instance.TryMakeNewAllowed(out Area_Allowed areaAllowed))
+                    areaAllowed.RenamableLabel = startingAreaName;
+                else
+                    Log.Error("Attempted to create more default areas than what is allowed.");
             }
-
         }
     }
 }
