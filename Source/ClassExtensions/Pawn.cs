4139e62f73cca4294f0b862a4642221df8fd1710
protobeard <michal.fouquette@gmail.com>
2025-11-01 12:23:02 -0700

    Update Pawn.IsSleepingAlone to use ThoughtWorker instead of copying its method

--- Source/ClassExtensions/Pawn.cs
+++ Source/ClassExtensions/Pawn.cs
@@ -3,6 +3,7 @@ using RimWorld;
 using RimWorld.Planet;
 using Verse;
 using PreceptDefOf = MadagascarVanilla.DefOfs.PreceptDefOf;
+using ThoughtDefOf = MadagascarVanilla.DefOfs.ThoughtDefOf;
 
 namespace MadagascarVanilla.ClassExtensions
 {
@@ -16,33 +17,9 @@ namespace MadagascarVanilla.ClassExtensions
         
         public static bool IsSleepingAlone(this Pawn p)
         {
-            List<DirectPawnRelation> list = LovePartnerRelationUtility.ExistingLovePartners(p, allowDead: false);
-            bool isSlaveOfColony = p.IsSlaveOfColony;
-            if (list.NullOrEmpty())
-            {
-                return false;
-            }
-            int num = 0;
-            for (int i = 0; i < list.Count; i++)
-            {
-                if (list[i].otherPawn.IsColonist && !list[i].otherPawn.IsWorldPawn() && list[i].otherPawn.relations.everSeenByPlayer && isSlaveOfColony == list[i].otherPawn.IsSlaveOfColony)
-                {
-                    if (p.ownership.OwnedBed != null && p.ownership.OwnedBed == list[i].otherPawn.ownership.OwnedBed)
-                    {
-                        return false;
-                    }
-                    if (p.ownership.OwnedRoom != null && p.ownership.OwnedRoom == list[i].otherPawn.ownership.OwnedRoom)
-                    {
-                        return false;
-                    }
-                    HistoryEventDef historyEventDef = ((list[i].def == PawnRelationDefOf.Spouse) ? HistoryEventDefOf.SharedBed_Spouse : HistoryEventDefOf.SharedBed_NonSpouse);
-                    if (new HistoryEvent(historyEventDef, p.Named(HistoryEventArgsNames.Doer)).DoerWillingToDo())
-                    {
-                        num++;
-                    }
-                }
-            }
-            return num > 0;
+            ThoughtWorker_WantToSleepWithSpouseOrLover worker = new ThoughtWorker_WantToSleepWithSpouseOrLover();
+            worker.def = ThoughtDefOf.WantToSleepWithSpouseOrLover;
+            return worker.CurrentState(p).Active;
         }
     }
 }
---
+++ Source/DefOfs/ThoughtDefOf.cs
@@ -0,0 +1,11 @@
+using RimWorld;
+using Verse;
+
+namespace MadagascarVanilla.DefOfs
+{
+    [DefOf]
+    public static class ThoughtDefOf
+    {
+        public static ThoughtDef WantToSleepWithSpouseOrLover;
+    }
+}
